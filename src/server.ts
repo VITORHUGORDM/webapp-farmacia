396f0c3a4835d0583c5324d0c2456257f5a3cb86
VITORHUGORDM <vitrovitinho77@gmail.com>
2025-11-19 17:33:21 -0300

    arrumando bugs

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on http://localhost:${PORT}`);
-});
